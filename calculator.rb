0feae6a1785d91b51ae18935218e99ebd759b112
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-30 18:40:45 -0500

    Added while loop with condition and fixed typos

--- calculator.rb
+++ calculator.rb
@@ -18,11 +18,21 @@ end
 #calc_method(4, "*", 4)
 #calc_method(4, "/", 4)
 
-p "Please enter something calculation"
-input = gets.chomp
-new_input = input.split(' ')
-int1 = new_input[0].to_i
-str = new_input[1]
-int2 = new_input[2].to_i
+input = " "
+counter = 0
+while input != "done"
+	p "Please enter something to calculate"
+	input = gets.chomp
+		if input == "done"
+			break
+		else
+			new_input = input.split(' ')
 
-calc_method(int1, str, int2)
+			int1 = new_input[0].to_i
+			str = new_input[1]
+			int2 = new_input[2].to_i
+
+			calc_method(int1, str, int2)
+		end
+
+end
