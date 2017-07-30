ea9d16ef0fca5a0d3bb73f688248892e1922dc9e
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-30 18:30:34 -0500

    Complete first task

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,19 @@
-Hello
+def calc_method(int1, str, int2)
+	answer = 0
+	if str == "+"
+		answer = int1 + int2
+	elsif  str == "-"
+		answer = int1 - int2
+	elsif str == "*"
+		answer = int1 * int2
+	else str == "/"
+		answer = int1 / int2
+	end
+
+	p answer
+end
+
+calc_method(4, "+", 4)
+calc_method(4, "-", 4)
+calc_method(4, "*", 4)
+calc_method(4, "/", 4)
