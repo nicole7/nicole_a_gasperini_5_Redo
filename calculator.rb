a2942bc7fbc47dcd1241ab5126063278d5379dbf
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-07-30 18:37:16 -0500

    Calculate when user inputs an expression

--- calculator.rb
+++ calculator.rb
@@ -13,7 +13,16 @@ def calc_method(int1, str, int2)
 	p answer
 end
 
-calc_method(4, "+", 4)
-calc_method(4, "-", 4)
-calc_method(4, "*", 4)
-calc_method(4, "/", 4)
+#calc_method(4, "+", 4)
+#calc_method(4, "-", 4)
+#calc_method(4, "*", 4)
+#calc_method(4, "/", 4)
+
+p "Please enter something calculation"
+input = gets.chomp
+new_input = input.split(' ')
+int1 = new_input[0].to_i
+str = new_input[1]
+int2 = new_input[2].to_i
+
+calc_method(int1, str, int2)
